a994bc34a116be5856e4ed811adc51ad586f7ab9
Jing <54467153+Alexcander-Jing@users.noreply.github.com>
2022-03-20 12:15:35 +0800

    Add files via upload

---
+++ DL_study/main.py
@@ -0,0 +1,13 @@
+import torch
+import numpy as np
+import matplotlib.pyplot as plt
+import argparse
+import os
+
+
+
+
+if __name__ == '__main__':
+    print("this is the test, code from LiMU AI")
+    # see the code and notes at https://courses.d2l.ai/zh-v2/
+
