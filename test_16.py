7811266e2b2a23db176223cb90d5283ca89affa8
Alexcander-Jing <18061523086@163.com>
2022-09-03 21:20:25 +0800

    test 16 model structure

--- test_16.py
+++ test_16.py
@@ -58,8 +58,51 @@ net = nn.Sequential(nn.Linear(4, 8), nn.ReLU(), nn.Linear(8, 1))
 X = torch.rand(size=(2, 4))
 print(net[2].state_dict())  # 显示最后一层的参数
 print(net[2].bias)  # 这里显示的是parameter参数
-print(net[2].bias.date)  # 这里显示的是数值本身
+print(net[2].bias.data)  # 这里显示的是数值本身
 print(net[2].weight.grad)  # 显示梯度gradient
 print(*[(name, param.shape) for name , param in net.named_parameters()])  # 显示里面对应层的weights 和 bias 参数
 print(net.state_dict()['2.bias'].data)  # 直接显示对应的层的参数
+
+def block1():
+    return nn.Sequential(nn.Linear(4, 8), nn.ReLU(), nn.Linear(8, 4), nn.ReLU())
+
+def block2():
+    net = nn.Sequential()
+    for i in range(4):
+        net.add_module(f'block {i}', block1())  # 使用类似字典的方式添加block，这里是添加了4次block1()结构
+    return net
+
+rgnet = nn.Sequential(block2(), nn.Linear(4, 1))
+# print(rgnet)
+
+def init_normal(m):
+    if type(m) == nn.Linear:
+        nn.init.normal_(m.weight, mean=0, std=0.01)
+        nn.init.zeros_(m.bias)
+# net.apply(init_normal)  # .apply() 遍历所有module，对其进行处理
+
+def init_constant(m):
+    if type(m) == nn.Linear:
+        nn.init.constant_(m.weight, 1)  # 将权重全部初始化为1常数
+        nn.init.zeros_(m.bias)
+
+def xavier(m):
+    if type(m) == nn.Linear:
+        nn.init.xavier_uniform_(m.weight)
+
+rgnet[0].apply(xavier)
+
+def my_init(m):
+    if type(m) == nn.Linear:
+        print("Init", *[(name, param.shape) for name, param in m.named_parameters()][0])
+    nn.init.uniform_(m.weight, -10, 10)
+    m.weight.data *= m.weight.data.abs() >= 5
+
+net[0].weight.data += 1.0
+net[0].bias.data += 1.0
+
+shared = nn.Linear(8, 8)
+net = nn.Sequential(nn.Linear(4, 8), nn.ReLU(), shared, nn.ReLU(), shared, nn.ReLU(), nn.Linear(8, 1))  # 两个shared层初始化的时候是共享权重的
+print(net[2].weight.data[0] == net[4].weight.data[0])
+
 
